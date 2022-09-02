366745c5eee0d652a5687b7a6bccf841ac67f9b1
AlejandroMartinez8 <Alejandro.Martinez8@upr.edu>
2022-09-02 07:54:53 -0400

    simple user name program

---
+++ practiceprelab.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+string name;
+
+cout <<"Hello User!\n";
+cout<<"PLease enter your name";
+getline(cin,name);
+
+cout <<"Hello, " << name << "!";
+    return 0;
+}
