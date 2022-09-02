123fffa798a8c4132402d515f8111bd23ae7d651
AlejandroMartinez8 <Alejandro.Martinez8@upr.edu>
2022-09-02 08:19:43 -0400

    added math question

--- practiceprelab.cpp
+++ practiceprelab.cpp
@@ -5,11 +5,35 @@ using namespace std;
 int main(){
 
 string name;
-
+char answer;
 cout <<"Hello User!\n";
-cout<<"PLease enter your name";
+cout<<"Please enter your name: ";
 getline(cin,name);
 
 cout <<"Hello, " << name << "!";
+    
+cout <<"\nWould you like to do some math?\n Enter Y for Yes and N for No\n";
+    cin>>answer;
+    
+    if(answer = 'Y'){
+double result; 
+cout << "\nwhat is 5 +10?\n";
+cin>> result;
+while(result!=15){
+cout <<"\nWrong! Try again.\n";
+
+cin>>result;
+
+
+}
+
+
+    }
+    
+    cout << "Correct the answer was 15!";
+    
+    
+    
+    
     return 0;
 }
